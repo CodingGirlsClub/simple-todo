072fb908060b8a91828f3e9d5e47aee0a3290798
lazybios <lazybios@gmail.com>
2016-08-20 22:30:21 +0800

    feat(model): 添加User模型
    rails generate model user username:string password_digest:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813023611) do
+ActiveRecord::Schema.define(version: 20160820142802) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20160813023611) do
     t.boolean  "is_finished"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
